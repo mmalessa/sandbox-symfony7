fa8589fd0493fd4caf6df79f0685277f3e651c6a
Marcin Malessa <marcin@malessa.pl>
2025-11-07 21:44:20 +0100

    Set up monolog

--- src/Application/Handler/DemoMessageHandler.php
+++ src/Application/Handler/DemoMessageHandler.php
@@ -5,19 +5,27 @@ declare(strict_types=1);
 namespace App\Application\Handler;
 
 use App\Application\Message\DemoMessage;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Contracts\Service\ResetInterface;
 
 #[AsMessageHandler]
 class DemoMessageHandler implements ResetInterface
 {
+    public function __construct(
+        private readonly LoggerInterface $logger,
+    )
+    {
+    }
+
     public function __invoke(DemoMessage $message)
     {
+        $this->logger->info('Demo message received');
         print_r($message);
     }
 
     public function reset()
     {
-        echo "## Handler reset ##\n";
+        $this->logger->info('Handler reset');
     }
 }
