6b4364cb1c8cfa6ec4b0133f5e72558bf04ba205
Marcin Malessa <marcin@malessa.pl>
2025-11-12 20:44:35 +0100

    Update MagicResetLever

--- src/Application/Handler/DemoMessageHandler.php
+++ src/Application/Handler/DemoMessageHandler.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Application\Handler;
 
 use App\Application\Message\DemoMessage;
+use App\Infrastructure\Symfony\MagicResetLever;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Contracts\Service\ResetInterface;
@@ -14,12 +15,14 @@ class DemoMessageHandler implements ResetInterface
 {
     public function __construct(
         private readonly LoggerInterface $logger,
+        private readonly MagicResetLever $magicResetLever
     )
     {
     }
 
     public function __invoke(DemoMessage $message)
     {
+        $this->magicResetLever->boo();
         $this->logger->info('Demo message received');
         print_r($message);
     }
--- src/Infrastructure/Symfony/MagicResetLever.php
+++ src/Infrastructure/Symfony/MagicResetLever.php
@@ -4,12 +4,21 @@ declare(strict_types=1);
 
 namespace App\Infrastructure\Symfony;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Contracts\Service\ResetInterface;
 
 class MagicResetLever implements ResetInterface
 {
+    public function __construct(
+        private readonly LoggerInterface $logger,
+    ) {}
+
+    public function boo(): void
+    {}
+
+
     public function reset(): void
     {
-        echo "############### BIG RESET LEVER ################\n";
+        $this->logger->info("############### MAGIC RESET LEVER ################");
     }
 }
